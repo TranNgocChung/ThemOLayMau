45a585ca663e8324b5320f230d2439b504611122
TranNgocChung <tranngocchung.ktv@gmail.com>
2024-04-10 15:56:15 +0700

    them dien dieu tri

--- HIS.Desktop.LocalStorage.BackendData.V2/EFMODEL/L_HIS_SERVICE_REQ_101.cs
+++ HIS.Desktop.LocalStorage.BackendData.V2/EFMODEL/L_HIS_SERVICE_REQ_101.cs
@@ -24,5 +24,6 @@ namespace HIS.Desktop.LocalStorage.BackendData.V2.EFMODEL
         public Nullable<long> PARENT_SERVICE_ID { get; set; }
         public string PARENT_SERVICE_CODE { get; set; }
         public string PARENT_SERVICE_NAME { get; set; }
+        public Nullable<long> TDL_TREATMENT_TYPE_ID { get; set; }
     }
 }
